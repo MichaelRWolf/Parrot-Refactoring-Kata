7b0e74e8465eaf07c536cd7004f99f66c26853ae
Michael R. Wolf <MichaelRWolf@att.net>
2024-02-20 20:19:24 -0500

    Add switch statement to create_parrot() for future biz logic

--- Python/Notes.md
+++ Python/Notes.md
@@ -1,3 +1,14 @@
 
 https://www.youtube.com/watch?v=IvFX8Ivit1k
 
+
+https://youtu.be/IvFX8Ivit1k?t=104
+1:44 Refactor > Convert > Constructor to Factory Method: 'CreateParrot(type, numberOfCoconuts, voltage, isNailed)'
+
+
+2:14 Add switch(type) statement to factory method
+
+3:08 commit "Add switch statement to factory method"
+
+
+
--- Python/Parrot-Refactoring-Kata/src/parrot.py
+++ Python/Parrot-Refactoring-Kata/src/parrot.py
@@ -17,8 +17,16 @@ class Parrot:
 
     @staticmethod
     def create_parrot(type_of_parrot, number_of_coconuts, voltage, nailed):
-        self = Parrot(type_of_parrot, number_of_coconuts, voltage, nailed)
-        return self
+        match type_of_parrot:
+            case  ParrotType.EUROPEAN:
+                parrot = Parrot(type_of_parrot, number_of_coconuts, voltage, nailed)
+            case ParrotType.AFRICAN:
+                parrot = Parrot(type_of_parrot, number_of_coconuts, voltage, nailed)
+            case ParrotType.NORWEGIAN_BLUE:
+                parrot = Parrot(type_of_parrot, number_of_coconuts, voltage, nailed)
+            case _:
+                raise ValueError(f"Illegal type {type_of_parrot}")
+        return parrot
 
     def speed(self):
         if self._type == ParrotType.EUROPEAN:
