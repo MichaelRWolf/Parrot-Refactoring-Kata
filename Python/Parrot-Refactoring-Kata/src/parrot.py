9f2b3c65f2a9076e87ffe64596f5589732648b15
Michael R. Wolf <MichaelRWolf@att.net>
2024-02-20 21:40:11 -0500

    r Reformatting

--- Python/Parrot-Refactoring-Kata/src/parrot.py
+++ Python/Parrot-Refactoring-Kata/src/parrot.py
@@ -69,11 +69,12 @@ class EuropeanParrot(Parrot):
     def __init__(self, number_of_coconuts: int, voltage: float, nailed: bool) -> None:
         super().__init__(ParrotType.EUROPEAN, number_of_coconuts, voltage, nailed)
 
+
 class AfricanParrot(Parrot):
     def __init__(self, number_of_coconuts: int, voltage: float, nailed: bool) -> None:
         super().__init__(ParrotType.AFRICAN, number_of_coconuts, voltage, nailed)
 
+
 class NorwegianBlueParrot(Parrot):
     def __init__(self, number_of_coconuts: int, voltage: float, nailed: bool) -> None:
         super().__init__(ParrotType.NORWEGIAN_BLUE, number_of_coconuts, voltage, nailed)
-
