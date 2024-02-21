0303a4327aa0044fb9b9002148daa04f27aa17b1
Michael R. Wolf <MichaelRWolf@att.net>
2024-02-20 22:25:33 -0500

    R Push down member: nailed

--- Python/Parrot-Refactoring-Kata/src/parrot.py
+++ Python/Parrot-Refactoring-Kata/src/parrot.py
@@ -9,19 +9,18 @@ class ParrotType(Enum):  # If it is not available, just remove it.
 
 
 class Parrot(ABC):
-    def __init__(self, type_of_parrot: ParrotType, number_of_coconuts: int, voltage: float, nailed: bool) -> None:
+    def __init__(self, type_of_parrot: ParrotType, number_of_coconuts: int, voltage: float) -> None:
         self._type = type_of_parrot
         self._number_of_coconuts = number_of_coconuts
         self._voltage = voltage
-        self._nailed = nailed
 
     @staticmethod
     def create_parrot(type_of_parrot: ParrotType, number_of_coconuts: int, voltage: float, nailed: bool) -> 'Parrot':
         match type_of_parrot:
             case ParrotType.EUROPEAN:
-                parrot = EuropeanParrot(number_of_coconuts, voltage, nailed)
+                parrot = EuropeanParrot(number_of_coconuts, voltage)
             case ParrotType.AFRICAN:
-                parrot = AfricanParrot(number_of_coconuts, voltage, nailed)
+                parrot = AfricanParrot(number_of_coconuts, voltage)
             case ParrotType.NORWEGIAN_BLUE:
                 parrot = NorwegianBlueParrot(number_of_coconuts, voltage, nailed)
             case _:
@@ -49,8 +48,8 @@ class Parrot(ABC):
 
 
 class EuropeanParrot(Parrot):
-    def __init__(self, number_of_coconuts: int, voltage: float, nailed: bool) -> None:
-        super().__init__(ParrotType.EUROPEAN, number_of_coconuts, voltage, nailed)
+    def __init__(self, number_of_coconuts: int, voltage: float) -> None:
+        super().__init__(ParrotType.EUROPEAN, number_of_coconuts, voltage)
 
     def speed(self) -> float:
         return self._base_speed()
@@ -60,8 +59,8 @@ class EuropeanParrot(Parrot):
 
 
 class AfricanParrot(Parrot):
-    def __init__(self, number_of_coconuts: int, voltage: float, nailed: bool) -> None:
-        super().__init__(ParrotType.AFRICAN, number_of_coconuts, voltage, nailed)
+    def __init__(self, number_of_coconuts: int, voltage: float) -> None:
+        super().__init__(ParrotType.AFRICAN, number_of_coconuts, voltage)
 
     def speed(self) -> float:
         return max(0.0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
@@ -72,7 +71,8 @@ class AfricanParrot(Parrot):
 
 class NorwegianBlueParrot(Parrot):
     def __init__(self, number_of_coconuts: int, voltage: float, nailed: bool) -> None:
-        super().__init__(ParrotType.NORWEGIAN_BLUE, number_of_coconuts, voltage, nailed)
+        super().__init__(ParrotType.NORWEGIAN_BLUE, number_of_coconuts, voltage)
+        self._nailed = nailed
 
     def speed(self) -> float:
         if self._nailed:
