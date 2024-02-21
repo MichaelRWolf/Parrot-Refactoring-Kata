9826ed328dd193964a56afac7d52038ef4f70d57
Michael R. Wolf <MichaelRWolf@att.net>
2024-02-20 22:33:19 -0500

    R Push down member (only to AfricanParrot): _number_of_coconuts

--- Python/Parrot-Refactoring-Kata/src/parrot.py
+++ Python/Parrot-Refactoring-Kata/src/parrot.py
@@ -9,9 +9,8 @@ class ParrotType(Enum):  # If it is not available, just remove it.
 
 
 class Parrot(ABC):
-    def __init__(self, type_of_parrot: ParrotType, number_of_coconuts: int, voltage: float) -> None:
+    def __init__(self, type_of_parrot: ParrotType, voltage: float) -> None:
         self._type = type_of_parrot
-        self._number_of_coconuts = number_of_coconuts
         self._voltage = voltage
 
     @staticmethod
@@ -49,7 +48,7 @@ class Parrot(ABC):
 
 class EuropeanParrot(Parrot):
     def __init__(self, number_of_coconuts: int, voltage: float) -> None:
-        super().__init__(ParrotType.EUROPEAN, number_of_coconuts, voltage)
+        super().__init__(ParrotType.EUROPEAN, voltage)
 
     def speed(self) -> float:
         return self._base_speed()
@@ -60,7 +59,8 @@ class EuropeanParrot(Parrot):
 
 class AfricanParrot(Parrot):
     def __init__(self, number_of_coconuts: int, voltage: float) -> None:
-        super().__init__(ParrotType.AFRICAN, number_of_coconuts, voltage)
+        super().__init__(ParrotType.AFRICAN, voltage)
+        self._number_of_coconuts = number_of_coconuts
 
     def speed(self) -> float:
         return max(0.0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
@@ -71,7 +71,7 @@ class AfricanParrot(Parrot):
 
 class NorwegianBlueParrot(Parrot):
     def __init__(self, number_of_coconuts: int, voltage: float, nailed: bool) -> None:
-        super().__init__(ParrotType.NORWEGIAN_BLUE, number_of_coconuts, voltage)
+        super().__init__(ParrotType.NORWEGIAN_BLUE, voltage)
         self._nailed = nailed
 
     def speed(self) -> float:
