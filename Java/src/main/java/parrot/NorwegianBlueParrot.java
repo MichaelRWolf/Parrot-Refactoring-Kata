0b38600da173b762ca8bcf8500a4c7e483100284
Michael R. Wolf <MichaelRWolf@att.net>
2024-02-22 20:18:57 -0500

    r Create sub-clases EuropeanParrot, AfricanParrot, NorwegianBlueParrot

---
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -0,0 +1,8 @@
+package parrot;
+
+public class NorwegianBlueParrot extends Parrot {
+  public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage,
+                             boolean isNailed) {
+    super(type, numberOfCoconuts, voltage, isNailed);
+  }
+}
--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -7,21 +7,21 @@ public class Parrot {
   private final double voltage;
   private final boolean isNailed;
 
-  private Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+  Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
     this.type = type;
     this.numberOfCoconuts = numberOfCoconuts;
     this.voltage = voltage;
     this.isNailed = isNailed;
   }
 
-  public static Parrot createParrot(ParrotTypeEnum type,
-                                    int numberOfCoconuts,
-                                    double voltage,
-                                    boolean isNailed) {
+  static Parrot createParrot(ParrotTypeEnum type,
+                             int numberOfCoconuts,
+                             double voltage,
+                             boolean isNailed) {
     return switch (type) {
-      case EUROPEAN -> new Parrot(type, numberOfCoconuts, voltage, isNailed);
-      case AFRICAN -> new Parrot(type, numberOfCoconuts, voltage, isNailed);
-      case NORWEGIAN_BLUE -> new Parrot(type, numberOfCoconuts, voltage, isNailed);
+      case EUROPEAN -> new EuropeanParrot(type, numberOfCoconuts, voltage, isNailed);
+      case AFRICAN -> new AfricanParrot(type, numberOfCoconuts, voltage, isNailed);
+      case NORWEGIAN_BLUE -> new NorwegianBlueParrot(type, numberOfCoconuts, voltage, isNailed);
     };
   }
 
