f922f450c70fe1d731ab5db34b4fc4719d057fe9
Michael R. Wolf <MichaelRWolf@att.net>
2024-02-23 12:43:20 -0500

    r Extract methods: createEuropeanParrot(), createAfricanParrot(), createNorwegianBlueParrot()

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -11,28 +11,35 @@ public abstract class Parrot {
                              double voltage,
                              boolean isNailed) {
     return switch (type) {
-      case EUROPEAN -> {
-        if (numberOfCoconuts != 0) {
-          throw new IllegalArgumentException(
-              String.format("numberOfCoconuts must be 0 for %s", type));
-        }
-        if (voltage != 0.0) {
-          throw new IllegalArgumentException(
-              String.format("voltage must be 0.0 for %s", type));
-        }
-        if (isNailed) {
-          throw new IllegalArgumentException(
-              String.format("isNailed must be false for %s", type));
-        }
-        yield new EuropeanParrot();
-      }
-      case AFRICAN -> {
-        yield new AfricanParrot(numberOfCoconuts);
-      }
-      case NORWEGIAN_BLUE -> {
-        yield new NorwegianBlueParrot(voltage, isNailed);
-      }
+      case EUROPEAN -> createEuropeanParrot(type, numberOfCoconuts, voltage, isNailed);
+      case AFRICAN -> createAfricanParrot(numberOfCoconuts);
+      case NORWEGIAN_BLUE -> createNorwegianBlueParrot(voltage, isNailed);
     };
+  }
+
+  private static NorwegianBlueParrot createNorwegianBlueParrot(double voltage, boolean isNailed) {
+    return new NorwegianBlueParrot(voltage, isNailed);
+  }
+
+  private static AfricanParrot createAfricanParrot(int numberOfCoconuts) {
+    return new AfricanParrot(numberOfCoconuts);
+  }
+
+  private static EuropeanParrot createEuropeanParrot(ParrotTypeEnum type, int numberOfCoconuts,
+                                                  double voltage, boolean isNailed) {
+    if (numberOfCoconuts != 0) {
+      throw new IllegalArgumentException(
+          String.format("numberOfCoconuts must be 0 for %s", type));
+    }
+    if (voltage != 0.0) {
+      throw new IllegalArgumentException(
+          String.format("voltage must be 0.0 for %s", type));
+    }
+    if (isNailed) {
+      throw new IllegalArgumentException(
+          String.format("isNailed must be false for %s", type));
+    }
+    return new EuropeanParrot();
   }
 
   public abstract double getSpeed();
