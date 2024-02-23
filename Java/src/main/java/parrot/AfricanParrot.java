e613056a9516c83be66e6cf1cb128220e8381652
Michael R. Wolf <MichaelRWolf@att.net>
2024-02-22 20:56:18 -0500

    R Remove 'type' parameter to sub-class constructors

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -1,11 +1,8 @@
 package parrot;
 
 public class AfricanParrot extends Parrot {
-  public AfricanParrot(ParrotTypeEnum type,
-                       int numberOfCoconuts,
-                       double voltage,
-                       boolean isNailed) {
-    super(type, numberOfCoconuts, voltage, isNailed);
+  public AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
+    super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, voltage, isNailed);
   }
 
   @Override
